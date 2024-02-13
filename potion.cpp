f741ef463f079701461dae14c43dd8972704b00e
Rushikesh Deshmukh <58228830+rushikdm@users.noreply.github.com>
2024-02-13 14:06:56 +0530

    Update potion.cpp

--- potion.cpp
+++ potion.cpp
@@ -1,22 +1,36 @@
 /**
  * @author : Rushikesh Keshavrao Deshmukh
  * @email  : rushissv@gmail.com
- *
- * Write a program to determine proportions for a potion.
- * The potion has n ingredients.
- * (n-1) inputs for the relative proportions of two ingredients are given as follows:
- *     index1, index2, amount1, amount2
- *
- *     index1 denotes the index of ingredient 1
- *     index2 denotes the index of ingredient 2
- *     amount1 denotes the amount of ingredient 1
- *     amount2 denotes the amount of ingredient 2
- *
- * Determine the proportions of all the ingredients if unique solution exists.
- * The proportions of all ingredients should add to 1.0 if solution exists
- * If solution does not exist or if there is no unique solution, all the proportions should be zero.
- *
- * The program should be able to determine if unique solution exists or not.
+ * @date   : 10-Feb-2024
+
+This program is written to solve the following problem:
+Consider a potion(recipe) with N ingredients.
+The objective is to determine the proportions of all the N ingredients in the potion.
+Input consists of (N-1) lines. Each line has four integer values as follows:
+
+	  index_i, index_j, amount_i, amount_j
+
+index_i denotes the index of ingredient i
+index_j denotes the index of ingredient j
+amount_i denotes the relative proportionate amount of ingredient i
+amount_j denotes the relative proportionate amount of ingredient j
+
+Where
+0 <= i, j < N
+0 < amount_i, amount_j < 10
+2 < N < 200
+
+Above four integer values indicate the relative proportions of the two ingredients i, j.
+Like this (N-1) inputs are given for different pair of ingredients.
+
+The objective is to determine the proportions of all the N ingredients in potion.
+If unique solution exists, then, the output for proportions of all N ingredients should add to 1.0
+
+The inputs may be invalid or are not sufficient to determine the unique solution.
+In such cases, the program should report that unique solution does not exist.
+Otherwise program should output the proportion values for all N ingredients.
+
+If solution exists, the final proportion values of all the N ingredients must satisfy all the (N-1) relative proportions given in the inputs.
 
 Sample input:
 
@@ -24,34 +38,19 @@ Sample input:
 0 1 9 4
 2 1 1 2
 
-3 value mentioned in first line is the total number of ingredients in potion.
-Second line indicates that the relative proportions of ingredients 0, 1 should be 9:4
-Similarly last line indicates that the relative proportions of ingredients 2, 1 should be 1:2
-The final proportions of all ingredients should respect all the given relative proportions if solution exists.
+First line in the input (3 in this case) indicates the total number of ingredients in potion.
+After the first line, (N-1) lines follow. In this case, (3-1) = 2 lines follow.
+Each line that follows specifies the relative proportion of two ingredients.
+Second line mentioned above specifies that the relative proportion of ingredients 0, 1 must be 9:4
+Similarly, last line specifies that the relative proportion of ingredients 2, 1 must be 1:2
+
 The answer to above problem is as follows:
 (9/15)    (4/15)    (2/15)
 0.6000    0.2667    0.1333
 
+Above final proportion values satisfy all the relative proportion values mentioned in the inputs.
 
-Constraints:
-2 < N < 200
-0 < P < 10
-
-Where N is the number of ingredients in potion.
-P is the proportion amount for the ingredient specified in the inputs.
-
-* Approach 1 : Depth first search traversal (DFS) algorithm on the graph of ingradients.
-
-This graph has ingredients as nodes and relative proportions given in input as edges between the nodes.
-Since there are N nodes and (N-1) edges, following properties can be deduced:
-   - For valid inputs, it must form a tree without cycles.
-   - For valid inputs, all the nodes must be connected.
-If above conditions are satisfied, then, problem can be solved and has unique solution.
-Otherwise, unique solution cannot be found.
-To perform depth first traversal, connections data-structure needs to be built using the inputs.
-This DFS algorithm is implemented using determineProportionsUsingDFS() function in this file.
-
-* Approach 2: Incremental formation & merging of disjoint sets for ingredients.
+Solution Approach: Incremental formation & merging of disjoint sets for ingredients.
 
 Assume the mathematical inductive step k where the existing proportions of all the ingredients in potion are properly defined.
 These ingredients may belong to one or multiple disjoint groups.
@@ -78,18 +77,15 @@ Following are the various possibilities:
 At the end if the the total number of unique groups in potion is not equal to one, report that no solution exists.
 Otherwise determine the proportions for each ingredient in potion based on its amount in potion.
 
-Approach 2 is implemented using determineProportions() function in this file.
+This approach is implemented in determineProportions() function in this file.
     Time complexity  = O(N^2)
     Space complexity = O(N)
-
 */
-
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <numeric>
 #include <assert.h>
+#include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -114,20 +110,6 @@ static istream& operator >> (istream& is, Proportion& proportion)
 
 	return is;
 }
-
-static bool determineProportionsUsingDFS
-(
-	const int N,
-	const vector<Proportion>& iProportions,
-	vector<double>& oProportions
-);
-
-static bool determineProportions
-(
-	const int N,
-	const vector<Proportion>& iProportions,
-	vector<double>& oProportions
-);
 
 class Potion
 {
@@ -276,11 +258,9 @@ private:
 				ingredient.group = toGroup;
 		}
 	}
-
-	friend ostream& operator << (ostream & os, const Potion & potion);
 };
 
-bool determineProportions
+static bool determineProportions
 (
 	const int N,
 	const vector<Proportion> & iProportions,
@@ -304,95 +284,6 @@ bool determineProportions
 	return true;
 }
 
-bool determineProportionsUsingDFS
-(
-	const int N,
-	const vector<Proportion> & iProportions,
-	vector<double> & oProportions
-)
-{
-	assert(iProportions.size() == (oProportions.size() - 1));
-	assert(N == (int)oProportions.size());
-
-	vector<vector<int>> connections(N);
-	for (int i = 0; i < (N - 1); ++i)
-	{
-		connections[iProportions[i].one.index].push_back(i);
-		connections[iProportions[i].two.index].push_back(i);
-	}
-
-	vector<Proportion::Ingredient> ingredients;
-	ingredients.reserve(N);
-	ingredients.push_back({ 0,1 });
-
-	vector<bool> visited(N);
-	visited[0] = true;
-
-	queue<int> q;
-	q.push(0);
-
-	int count = 1;
-	while (!q.empty())
-	{
-		const int index = q.front();
-		q.pop();
-
-		for (int i = 0; i < (int)connections[index].size(); ++i)
-		{
-			const Proportion& proportion = iProportions[connections[index][i]];
-
-			const Proportion::Ingredient& current = (index == proportion.one.index) ? proportion.one : proportion.two;
-			assert(current.index == index);
-
-			const Proportion::Ingredient & adj = (index == proportion.one.index) ? proportion.two : proportion.one;
-
-			if (visited[adj.index])
-				continue;
-
-			q.push(adj.index);
-			visited[adj.index] = true;
-
-			int currentAmount = -1;
-
-			for (int j = 0; j < (int)ingredients.size(); ++j)
-			{
-				if (ingredients[j].index == index)
-					currentAmount = ingredients[j].amount;
-				ingredients[j].amount *= current.amount;
-			}
-
-			assert(currentAmount > 0);
-			ingredients.push_back({ adj.index, adj.amount * currentAmount });
-
-			++count;
-		}
-	}
-
-	if (count != N)
-		return false;
-
-	assert(N == (int)ingredients.size());
-	auto sort_lambda = [](const Proportion::Ingredient & ingredient1, const Proportion::Ingredient & ingredient2) -> bool {
-		return ingredient1.index < ingredient2.index;
-	};
-	std::sort(ingredients.begin(), ingredients.end(), sort_lambda);
-
-	for (int i = 0; i < N; ++i)
-	{
-		if (ingredients[i].index != i)
-			return false;
-	}
-
-	int sum = 0;
-	for (int i = 0; i < N; ++i)
-		sum += ingredients[i].amount;
-
-	for (int i = 0; i < N; ++i)
-		oProportions[i] = (double)ingredients[i].amount / sum;
-
-	return true;
-}
-
 int main()
 {
 	int N;
@@ -404,8 +295,7 @@ int main()
 		cin >> iProportions[i];
 
 	vector<double> oProportions(N);
-	//const bool success = determineProportions(N, iProportions, oProportions);
-	const bool success = determineProportionsUsingDFS(N, iProportions, oProportions);
+	const bool success = determineProportions(N, iProportions, oProportions);
 
 	if (!success)
 	{
